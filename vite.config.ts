0032f4d3d756355c1449188c9b0ada42851bb9b0
3str-webdev <3str.webdev@gmail.com>
2024-05-28 22:29:34 +0300

    fix: CI settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    }
-  },
-  plugins: [react()],
-})
+	base: "/monopoly-money/",
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src"),
+		},
+	},
+	plugins: [react()],
+});
